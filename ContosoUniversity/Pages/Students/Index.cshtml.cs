6fb3e9658b816f059ba231423e8774b3b80d539d
Eoin <mcleanee@hendrix.edu>
2026-03-15 19:19:45 -0500

    Add Age sorting to Students page

--- ContosoUniversity/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -19,12 +19,44 @@ namespace ContosoUniversity.Pages_Students
             _context = context;
             
         }
+        
+        public string NameSort { get; set; } = "";
+        public string DateSort { get; set; } = "";
+        public string AgeSort { get; set; } = "";
 
         public IList<Student> Student { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
-            Student = await _context.Student.ToListAsync();
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            AgeSort = sortOrder == "Age" ? "age_desc" : "Age";
+
+            IQueryable<Student> students = _context.Students;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    students = students.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    students = students.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    students = students.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                case "Age":
+                    students = students.OrderBy(s => s.Age);
+                    break;
+                case "age_desc":
+                    students = students.OrderByDescending(s => s.Age);
+                    break;
+                default:
+                    students = students.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            Student = await students.ToListAsync();
         }
     }
 }
